12f2fb672be0e3985b2a3f1d7aaaa1a0fa80f26f
pablogerez <pablogerez2000@gmail.com>
2020-03-11 00:21:51 +0100

    init

--- index.js
+++ index.js
@@ -1,22 +1,41 @@
 const superagent = require('superagent');
+const fs = require('fs');
 
-function imprimirMuseos(error, respuesta) {
-  if (error) {
+function escribirArchivo(error, respuesta) {
+    if (error) {
     throw new Error('algo se rompió', error);
   }
 
   const cantidad = respuesta.body.count;
   const museos = respuesta.body.results;
 
-  console.log(`Se encontraron ${cantidad} museos.`);
-  console.log(`El primer museo se llama ${museos[0].nombre}.`)
+  var actual = ""
+
+  for(var i = 0; i < cantidad; i ++) {
+    var museo = museos[i]
+    actual = `El primer museo se llama: ${museo.nombre}.`;
+  }
+  fs.writeFile ('./museos.txt',actual,end);
 }
 
-console.log('Antes de llamar a superagent')
+function end(error){
+  if (error) {
+    throw new Error('algo se rompió', error);
+  }
+  else{
+    console.log('Terminado.')
+  }
+}
+
+//console.log('Antes de llamar a superagent')
+
+
 
 superagent
   .get('https://www.cultura.gob.ar/api/v2.0/museos')
   .query({ format: 'json' })
-  .end(imprimirMuseos)
+  .end(escribirArchivo)
 
-console.log('Después de llamar a superagent')
+
+
+//console.log('Después de llamar a superagent')
