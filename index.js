07d5ab985e01bda3160bb737bf7cb20382711382
pablogerez <pablogerez2000@gmail.com>
2020-03-13 00:56:51 +0100

    museos y organismos

--- index.js
+++ index.js
@@ -1,41 +1,51 @@
 const superagent = require('superagent');
 const fs = require('fs');
 
-function escribirArchivo(error, respuesta) {
-    if (error) {
-    throw new Error('algo se rompió', error);
-  }
+function escribirArchivo(error) {
+  if (error) { throw new Error('algo se rompió', error); }
+  fs.writeFile('./museos.txt',"Museos: \n", getValuesMuseums);
+  fs.writeFile('./organismos.txt',"Organismos: \n",getValuesOrganisms);
+}
 
-  const cantidad = respuesta.body.count;
+function appendMuseosAlArchivo(error, respuesta) {
+  if (error) { throw new Error('algo se rompió', error); }
   const museos = respuesta.body.results;
 
-  var actual = ""
+  museos.forEach(museo => { 
+    actual = `${museo.nombre} (${museo.direccion}). Por cualquier consulta comunicarse al ${museo.telefono}\n`
+    fs.appendFile('./museos.txt',actual, end);
+  });
+}
 
-  for(var i = 0; i < cantidad; i ++) {
-    var museo = museos[i]
-    actual = `El primer museo se llama: ${museo.nombre}.`;
-  }
-  fs.writeFile ('./museos.txt',actual,end);
+function appendOrganismosAlArchivo(error, respuesta) {
+  if (error) { throw new Error('algo se rompió', error); }  
+  const organismos = respuesta.body.results;
+
+  organismos.forEach(organismo => { 
+    actual = `Organismo: ${organismo.nombre} (${organismo.direccion}). Por cualquier consulta comunicarse al ${organismo.telefono}\n`
+    fs.appendFile('./organismos.txt',actual, end);
+  });
 }
 
 function end(error){
-  if (error) {
-    throw new Error('algo se rompió', error);
-  }
-  else{
-    console.log('Terminado.')
-  }
+  if (error) { throw new Error('algo se rompió', error); }
+  else { console.log('Terminado.'); }
 }
 
-//console.log('Antes de llamar a superagent')
+function getValuesMuseums(error){
+  if (error) { throw new Error('algo se rompió', error);}
+  superagent
+    .get('https://www.cultura.gob.ar/api/v2.0/museos')
+    .query({ format: 'json' })
+    .end(appendMuseosAlArchivo)
+}
 
+function getValuesOrganisms(error) {
+  if (error) { throw new Error('algo se rompió', error);}
+  superagent
+  .get('https://www.cultura.gob.ar/api/v2.0/organismos')
+    .query({ format: 'json' })
+    .end(appendOrganismosAlArchivo)
+}
 
-
-superagent
-  .get('https://www.cultura.gob.ar/api/v2.0/museos')
-  .query({ format: 'json' })
-  .end(escribirArchivo)
-
-
-
-//console.log('Después de llamar a superagent')
+escribirArchivo();
